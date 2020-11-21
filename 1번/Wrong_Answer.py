3b15080501b60452f04436753eb6fe765701e6a5
Integral76 <31675804+jee00609@users.noreply.github.com>
2020-11-21 20:52:29 +0900

    오답

--- 1번/Wrong_Answer.py
+++ 1번/Wrong_Answer.py
@@ -1,3 +1,5 @@
+# 같이 동봉한 입력값을 넣을 시 정답과 결과값이 다른 것을 알 수 있다.
+
 class Solution:
     def solution(self, scores):
         #scores.sort()
@@ -7,9 +9,7 @@ class Solution:
         if len(scores)>1:
             scores.remove(max(scores)) # 최대값 제거
             scores.remove(min(scores)) # 최소값 제거
-        print(scores)
-        print(sum(scores))
-        print(len(scores))
+            
         avg = sum(scores)/len(scores) #평균
         
         return avg
